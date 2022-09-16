182995d6eeddb88481d5a96eb04a4a72dcc023b1
Richard Johnson <richcj10@gmail.com>
2022-09-15 22:40:51 -0500

    LTE Code Work

---
+++ LTE/src/Hardware/LED.cpp
@@ -0,0 +1,24 @@
+#include "LED.h"
+#include <FastLED.h>
+
+#define NUM_LEDS 1
+
+#define DATA_PIN 3
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void LEDsetup() {
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+}
+
+void LEDloop() {
+  // Turn the LED on, then pause
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
