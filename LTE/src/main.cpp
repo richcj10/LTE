e46c4a0dd15eaf431b5678ba442460bf24857bb1
Richard Johnson <richcj10@gmail.com>
2022-09-16 00:45:38 -0500

    Working begin

--- LTE/src/main.cpp
+++ LTE/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 #include "Functions.h"
+#include "Hardware/LED.h"
 
 void setup() {
   Startup();
 }
 
 void loop() {
+  RunLoop();
+  delay(1000);
+  DebugPrint();
+  LEDloop();
   // put your main code here, to run repeatedly:
 }
